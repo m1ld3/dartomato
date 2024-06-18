1c1cacd566ce494965a16e0fb107437a09327cf9
Michael Mildner <michael.mildner92@gmx.de>
2024-06-18 22:31:16 +0200

    Prepare sample data

--- tests/fixtures.h
+++ tests/fixtures.h
@@ -117,4 +117,36 @@ class CStatsWindowX01Test : public ::testing::Test
 {
 protected:
 
+  CStatsWindowX01Test()
+  {
+    double avg1Dart = 16.791666666666668;
+    double avg3Dart = 50.375;
+    double checkoutRate = 27.27272727272727;
+    double first9Avg = 40.833333333333336;
+    bool active = false;
+    uint32_t checkoutAttempts = 11;
+    uint32_t checkoutHits = 3;
+    uint32_t legsWonPerset = 1;
+    uint32_t remainingPoints = 392;
+    uint32_t setsWon = 1;
+    uint32_t totalDarts = 96;
+    uint32_t totalLegsWon = 3;
+    QVector<uint32_t> allScoresFlat = { 98, 26, 37, 45, 41, 65, 60, 46, 39, 20, 18, 6, 60, 60, 41, 100, 85, 41, 58, 56, 45, 3, 11, 30, 60, 79, 59, 58, 100, 44, 12, 60, 49};
+    QVector<uint32_t> remainingPointsOfCurrentLeg = {501, 441, 392};
+    QVector<uint32_t> scoresOfCurrentLeg = {60, 49};
+    QVector<QVector<uint32_t>> allScoresOfAllLegs = {{ 98, 26, 37, 45, 41, 65, 60, 46, 39, 20, 18, 6}, {60, 60, 41, 100, 85, 41, 58, 56}, {45, 3, 11, 30, 60, 79, 59, 58, 100, 44, 12}};
+    QVector<QVector<uint32_t>> remainingPointsOfAllLegs = {{501, 403, 377, 340, 295, 254, 189, 129, 83, 44, 24, 6, 0}, {501, 441, 381, 340, 240, 155, 114, 56, 0}, {501, 456, 453, 442, 412, 352, 273, 214, 156, 56, 12, 0}};
+
+    QVector<QVector<QVector<QString>>> thrownDartsOfAllLegs = {{{"t60", "s20", "s18"}, {"s1", "s20", "s5"}, {"s12", "s5", "s20"}, {"s20", "s20", "s5"}, {"s20", "s20", "s1"}, {"s25", "s20", "s20"}, {"s20", "s20", "s20"}, {"d40", "s5", "s1"}, {"s18", "s1", "s20"}, {"s18", "s1", "s1"}, {"s12", "s0", "s6"}, {"s0", "d6"}},
+                                                               {{"s20", "s20", "s20"}, {"s20", "s20", "s20"}, {"s20", "s20", "s1"}, {"s20", "t60", "s20"}, {"s20", "s5", "t60"}, {"s20", "s20", "s1"}, {"s20", "s18", "s20"}, {"s16", "s20", "d20"}},
+                                                               {{"s5", "s20", "s20"}, {"s1", "s1", "s1"}, {"s5", "s5", "s1"}, {"s5", "s5", "s20"}, {"s20", "s20", "s20"}, {"t57", "s19", "s3"}, {"s20", "s20", "s19"}, {"s20", "s19", "s19"}, {"s20", "s20", "t60"}, {"s16", "s16", "s12"}, {"d12"}}};
+    QVector<QVector<QString>> thrownDartsOfAllLegsFlat = {{"t60", "s20", "s18"}, {"s1", "s20", "s5"}, {"s12", "s5", "s20"}, {"s20", "s20", "s5"}, {"s20", "s20", "s1"}, {"s25", "s20", "s20"}, {"s20", "s20", "s20"}, {"d40", "s5", "s1"}, {"s18", "s1", "s20"}, {"s18", "s1", "s1"}, {"s12", "s0", "s6"}, {"s0", "d6"},
+                                                          {"s20", "s20", "s20"}, {"s20", "s20", "s20"}, {"s20", "s20", "s1"}, {"s20", "t60", "s20"}, {"s20", "s5", "t60"}, {"s20", "s20", "s1"}, {"s20", "s18", "s20"}, {"s16", "s20", "d20"},
+                                                          {"s5", "s20", "s20"}, {"s1", "s1", "s1"}, {"s5", "s5", "s1"}, {"s5", "s5", "s20"}, {"s20", "s20", "s20"}, {"t57", "s19", "s3"}, {"s20", "s20", "s19"}, {"s20", "s19", "s19"}, {"s20", "s20", "t60"}, {"s16", "s16", "s12"}, {"d12"}, {"s20", "s20", "s20"}, {"s15", "s17", "s17"}};
+    QVector<QVector<QString>> thrownDartsOfCurrentLeg = {{"s20", "s20", "s20"}, {"s15", "s17", "s17"}};
+
+    mData = {setsWon, legsWonPerset, totalLegsWon, remainingPoints, checkoutAttempts, checkoutHits, totalDarts, avg1Dart, avg3Dart, checkoutRate, first9Avg, scoresOfCurrentLeg, allScoresOfAllLegs, allScoresFlat, thrownDartsOfCurrentLeg, thrownDartsOfAllLegsFlat, thrownDartsOfAllLegs, remainingPointsOfCurrentLeg, remainingPointsOfAllLegs, active};
+  }
+
+  CX01Class::CPlayerData mData;
 };
