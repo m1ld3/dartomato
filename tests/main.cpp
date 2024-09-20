24ed119f020f21439366c3c6195681e62318362f
m1ld3 <74381402+m1ld3@users.noreply.github.com>
2024-09-20 20:36:02 +0200

    Update main.cpp

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,36 +1,36 @@
 #include "fixtures.h"
 #include <initializer_list>
 
-TEST_F(CDartBoardX01Test, InitTest)
-{
-  EXPECT_CALL(*mMockWindow.get(), erase_dart1());
-  EXPECT_CALL(*mMockWindow.get(), erase_dart2());
-  EXPECT_CALL(*mMockWindow.get(), erase_dart3());
-  EXPECT_CALL(*mMockWindow.get(), display_score(501));
-  mDartBoard->init_dartboard(501);
-  EXPECT_TRUE(verify_dartboard_x01({false, false, 501, 501, 3, {false, false, false}, {}, {0, 0, 0}}));
-}
+// TEST_F(CDartBoardX01Test, InitTest)
+// {
+//   EXPECT_CALL(*mMockWindow.get(), erase_dart1());
+//   EXPECT_CALL(*mMockWindow.get(), erase_dart2());
+//   EXPECT_CALL(*mMockWindow.get(), erase_dart3());
+//   EXPECT_CALL(*mMockWindow.get(), display_score(501));
+//   mDartBoard->init_dartboard(501);
+//   EXPECT_TRUE(verify_dartboard_x01({false, false, 501, 501, 3, {false, false, false}, {}, {0, 0, 0}}));
+// }
 
-TEST_F(CDartBoardX01Test, NoScoreAtBeginTest)
-{
-  mDartBoard->init_dartboard(501);
-  set_x01_mode(EGame::GAME_501);
-  QChar type = 's';
-  EXPECT_CALL(*mMockWindow.get(), display_score(501));
-  mDartBoard->handle_segment_pressed_event(0, type);
-  EXPECT_TRUE(verify_dartboard_x01({false, false, 501, 501, 2, {false, false, false}, {"s0"}, {0, 0, 0}}));
-}
+// TEST_F(CDartBoardX01Test, NoScoreAtBeginTest)
+// {
+//   mDartBoard->init_dartboard(501);
+//   set_x01_mode(EGame::GAME_501);
+//   QChar type = 's';
+//   EXPECT_CALL(*mMockWindow.get(), display_score(501));
+//   mDartBoard->handle_segment_pressed_event(0, type);
+//   EXPECT_TRUE(verify_dartboard_x01({false, false, 501, 501, 2, {false, false, false}, {"s0"}, {0, 0, 0}}));
+// }
 
-TEST_F(CDartBoardX01Test, AlreadyFinishedTest)
-{
-  mDartBoard->init_dartboard(501);
-  set_x01_mode(EGame::GAME_501);
-  mDartBoard->set_finished();
-  QChar type = 's';
-  mDartBoard->handle_segment_pressed_event(20, type);
-  EXPECT_TRUE(verify_dartboard_x01({false, false, 501, 501, 3, {false, false, false}, {}, {0, 0, 0}}));
-  EXPECT_TRUE(verify_dartboard_x01_warning(": Game already finished!"));
-}
+// TEST_F(CDartBoardX01Test, AlreadyFinishedTest)
+// {
+//   mDartBoard->init_dartboard(501);
+//   set_x01_mode(EGame::GAME_501);
+//   mDartBoard->set_finished();
+//   QChar type = 's';
+//   mDartBoard->handle_segment_pressed_event(20, type);
+//   EXPECT_TRUE(verify_dartboard_x01({false, false, 501, 501, 3, {false, false, false}, {}, {0, 0, 0}}));
+//   EXPECT_TRUE(verify_dartboard_x01_warning(": Game already finished!"));
+// }
 
 // TEST_F(CDartBoardX01Test, StartValSingleInTest)
 // {
